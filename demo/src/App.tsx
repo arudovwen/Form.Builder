c9980d77ae6f242a27b28e68a43c7b4204a2ec8f
Success Ahon <successahon@gmail.com>
2025-04-13 01:56:38 +0100

    Update App.tsx

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -2,35 +2,40 @@ import { Routes, Route } from "react-router";
 import { FormBuilder, FormViewer } from "@arudovwen/form-builder-react";
 import "@arudovwen/form-builder-react/dist/index.css";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  let form_data;
-  let loading = true;
-  try {
-    const storedData = localStorage.getItem("formData");
-    form_data = storedData ? JSON.parse(storedData) : null;
-  } catch (error) {
-    console.error("Error parsing formData from localStorage:", error);
-    form_data = null;
-  }
+  const [formData, setFormData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    try {
+      const storedData = localStorage.getItem("formData");
+      setFormData(storedData ? JSON.parse(storedData) : null);
+    } catch (error) {
+      console.error("Error parsing formData from localStorage:", error);
+      setFormData(null);
+    }
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+
+    return () => clearTimeout(timer); // Cleanup timeout on unmount
+  }, []);
+
   const config = {
     buttonColor: "#333",
     loaderColor: "#333",
   };
 
-  setTimeout(() => {
-    loading = false;
-  }, 4000);
   return (
     <Routes>
       <Route
         path="/"
         element={
           <FormBuilder
-            onSubmit={function (e: any): void {
-              console.log(e);
-            }}
+            onSubmit={(e: any) => console.log(e)}
             config={config}
           />
         }
@@ -39,10 +44,8 @@ function App() {
         path="/viewer"
         element={
           <FormViewer
-            onSubmit={function (e: any): void {
-              console.log(e);
-            }}
-            form_data={form_data}
+            onSubmit={(e: any) => console.log(e)}
+            form_data={formData}
             config={config}
             loading={loading}
           />
