6cf354e6051a2dfc9d3bf851cb268b5123b7bea4
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2025-05-22 03:29:28 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -218,7 +218,7 @@ function App() {
     buttonColor: "green",
     loaderColor: "green",
     elementColor:"green",
-    elementBgColor:"green",
+    elementBgColor:"",
     elementBorderColor: "green"
   };
   return (
