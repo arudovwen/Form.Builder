bc180c53ba211bb0bc1003254de7c69acbfa5abd
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2025-06-11 14:21:23 +0100

    UPDATE

--- types/components/editor/layout.d.ts
+++ types/components/editor/layout.d.ts
@@ -1,12 +1,12 @@
 import "react-toastify/dist/ReactToastify.css";
 export interface BuilderProps {
-    onSubmit: (e: any) => void;
+    onSubmit?: (e: any) => void;
     questionData?: any;
     isReadOnly?: boolean;
     config?: any;
     title?: string;
     loading?: boolean;
     goBackUrl?: () => void;
-    onPublish: (e: any) => void;
+    onPublish?: (e: any) => void;
 }
 export default function Layout({ onSubmit, questionData, title, goBackUrl, loading, onPublish, }: BuilderProps): import("react/jsx-runtime").JSX.Element;
