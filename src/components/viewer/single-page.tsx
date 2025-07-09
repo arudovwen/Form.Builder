8259e93112f4c9c94fce2f34f945d5b35006c168
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2025-07-09 02:03:43 +0100

    Update single-page.tsx

--- src/components/viewer/single-page.tsx
+++ src/components/viewer/single-page.tsx
@@ -4,13 +4,25 @@ import { renderElement } from "./elements-render";
 
 export default function SinglePage({ form_data, options }) {
   return (
-    <div className="grid gap-y-3">
-      {form_data.flatMap((section: any) =>
-        section.questionData?.map((element: any) => {
-          if (element.type === "grid") {
-            const gridChildren = section.questionData.filter(
-              (child: any) => child.gridId === element.id
-            );
+    <div className="grid gap-y-10">
+      {form_data.flatMap((section: any) => (
+        <div key={section.id} className="pb-6 border-b border-gray-100 last:border-none">
+          <div className="mb-4">
+            {section.title && (
+              <h2 className="text-lg font-semibold mb-[4px]">
+                {section?.title}
+              </h2>
+            )}
+            {section.description && (
+              <p className="text-sm">{section?.description}</p>
+            )}
+          </div>
+          <div className="grid gap-y-3">
+            {section.questionData?.map((element: any) => {
+              if (element.type === "grid") {
+                const gridChildren = section.questionData.filter(
+                  (child: any) => child.gridId === element.id
+                );
 
                 return (
                   <GridInput
@@ -31,22 +43,22 @@ export default function SinglePage({ form_data, options }) {
                 );
               }
 
-          if (!element.gridId) {
-            return (
-              <div
-                key={element.id}
-                className={clsx(
-                  "group relative grid gap-y-[6px]",
-                  element.elementClass
-                )}
-              >
-                {renderElement(element, options)}
-                <div className="mt-1 text-xs text-red-600">
-                  {options?.errors?.[element.id]?.message}
-                </div>
-              </div>
-            );
-          }
+              if (!element.gridId) {
+                return (
+                  <div
+                    key={element.id}
+                    className={clsx(
+                      "group relative grid gap-y-[6px]",
+                      element.elementClass
+                    )}
+                  >
+                    {renderElement(element, options)}
+                    <div className="mt-1 text-xs text-red-600">
+                      {options?.errors?.[element.id]?.message}
+                    </div>
+                  </div>
+                );
+              }
 
               return null;
             })}
